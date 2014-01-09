3addc06daf01c41abf868494c600d8028aca11ab
Cody Bonney <me@codybonney.com>
2014-01-09 15:33:52 -0700

    retrieves raw body of request

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 var express = require('express');
 var app = express();
 
-app.post('/', function(req, res) {
-	res.send('hello world');
+app.use (function(req, res, next) {
+    var data='';
+    req.setEncoding('utf8');
+    req.on('data', function(chunk) {
+       data += chunk;
+    });
+
+    req.on('end', function() {
+        req.body = data;
+        next();
+    });
+});
+
+app.post('/', function(req, res)
+{
+    console.log(req.body);
+	res.send('recieved data');
 });
 
 app.listen(3000);
