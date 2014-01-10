25bbe53d644692a80d797e9f1bda90b44e0fdea7
Cody Bonney <me@codybonney.com>
2014-01-10 08:50:16 -0700

    removed old commented out code

--- app.js
+++ app.js
@@ -90,42 +90,4 @@ app.post('/', function(req, res)
 
 });
 
-/*
-app.post('/', function(req, res)
-{
-	var sketch = req.body;
-	var directory = './compiled/' + randomString(25);
-
-	// write sketch data to sketch.ino
-	fs.writeFile("./src/sketch.ino", sketch, function(err) {
-	    if(err) {
-	        console.log(err);
-		    res.send('error: ' + err);
-	    } else {
-
-		    // sketch was saved to sketch.ino
-		    // build firmware
-		    child = exec('./compile.sh ' + directory, function (error, stdout, stderr) {
-		        console.log('stdout: ' + stdout);
-			    if(stderr) {
-				    res.send('error: ' + stderr);
-			    } else {
-
-				    // read compiled file
-				    fs.readFile('./.build/uno/firmware.hex', 'utf8', function (err, data) {
-					if (err) {
-						console.log(err);
-						res.send('error: ' + err);
-					}
-						res.send(data);
-				    })
-
-		        }
-		   	});
-
-	    }
-	});
-});
-*/
-
 app.listen(3000);
