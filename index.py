d82d880537c4c8966f59b55e33a621af9dacf52a
Cody Bonney <me@codybonney.com>
2014-01-22 15:12:06 -0700

    Added init

--- index.py
+++ index.py
@@ -1,10 +1,18 @@
 from flask import Flask, request, render_template
 
-app = Flask(__name__)
+import sys
+import os.path
+import ino.runner
 
+app = Flask(__name__)
 
 @app.route('/compile', methods=['POST'])
 def compile_sketch():
+    compiled = "/Users/cody/Projects/github/arduino-cloud-compiler/compiled"
+
+    os.chdir(compiled)
+    ino.runner.main(['ino', 'init'])
+
     return 'compiling'
 
 if __name__ == '__main__':
