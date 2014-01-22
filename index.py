948da72c084f625174fd9c94d751ae1c7d6560be
Cody Bonney <me@codybonney.com>
2014-01-22 13:37:44 -0700

    routing and basic template

--- index.py
+++ index.py
@@ -1,12 +1,13 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
-def login():
+@app.route('/<name>', methods=['GET', 'POST'])
+def login(name=None):
     if request.method == 'POST':
         return 'POST request'
     else:
-        return 'GET request'
+        return render_template('landing.html', name=name)
 
 if __name__ == '__main__':
     app.run(debug=True)
