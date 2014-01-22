c3868aef6eaa56c4f8087236cab3f52d17907ab5
Cody Bonney <me@codybonney.com>
2014-01-22 13:55:55 -0700

    compile route added

--- index.py
+++ index.py
@@ -2,13 +2,10 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-@app.route('/<name>', methods=['GET', 'POST'])
-def login(name=None):
-    if request.method == 'POST':
-        return 'POST request'
-    else:
-        return render_template('landing.html', name=name)
+
+@app.route('/compile', methods=['POST'])
+def compile_sketch():
+    return 'compiling'
 
 if __name__ == '__main__':
     app.run(debug=True)
