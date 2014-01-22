31099882840d337b344612c2038c6e93d077518e
Cody Bonney <me@codybonney.com>
2014-01-22 13:22:03 -0700

    basic requests

--- index.py
+++ index.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return 'POST request'
+    else:
+        return 'GET request'
 
 if __name__ == '__main__':
     app.run(debug=True)
