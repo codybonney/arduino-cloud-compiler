396006d4ffea9e9639648973fd21df1ea9f20635
Cody Bonney <me@codybonney.com>
2014-01-22 15:52:38 -0700

    Added submission form skeleton

--- index.py
+++ index.py
@@ -10,15 +10,30 @@ def random_string(size=8, chars=string.ascii_uppercase + string.digits):
 
 app = Flask(__name__)
 
+@app.route('/')
+def submission():
+    return render_template('landing.html')
+
+
 @app.route('/compile', methods=['POST'])
 def compile_sketch():
     sketch_id = random_string()
     compiled_dir = "/Users/cody/Projects/github/arduino-cloud-compiler/compiled/" + sketch_id
 
+    # create a directory for the compiled sketch
     os.mkdir(compiled_dir)
     os.chdir(compiled_dir)
 
+    # initialize
     ino.runner.main(['ino', 'init'])
+
+    # write the sketch.ino file
+    sketch_file = open("src/sketch.ino", "w+")
+    sketch_file.write(request.form["sketch"])
+    sketch_file.close()
+
+    # build the project
+    ino.runner.main(['ino', 'build'])
 
     return 'compiling'
 
