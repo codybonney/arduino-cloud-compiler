7104d228d5d83be69d6ebb4f63926ad416a05909
Cody Bonney <me@codybonney.com>
2014-01-23 15:57:06 -0700

    Moved JS to controller

--- static/js/controller.js
+++ static/js/controller.js
@@ -1,14 +1,33 @@
 function Controller($scope) {
+	var $submitButton = $("#submit");
+	var $compileTime = $("#compileTime");
+	var $sketch = $("#sketch");
+	var $compiled = $("#compiled");
+
 	$scope.master = {};
 	$scope.sketch = "#define LED_PIN 13\n\nvoid setup()\n{\n    pinMode(LED_PIN, OUTPUT);\n}\n\nvoid loop()\n{\n    digitalWrite(LED_PIN, HIGH);\n    delay(100);\n    digitalWrite(LED_PIN, HIGH);\n    delay(900);\n}";
+	$scope.submit_sketch = "TEST";
 
-	$scope.update = function(user) {
-		$scope.master = angular.copy(user);
+	$scope.compile = function() {
+		$compileTime.html('Compiling...');
+		$compiled.val("");
+		$submitButton.val("Compiling Sketch...").addClass('compiling');
+
+		var compileTimeStart = new Date().getTime();
+
+		$.ajax({
+			type: "POST",
+			url: "./compile",
+			data: {
+			sketch: $sketch.val()
+			}
+			})
+			.done(function(hex) {
+			var compileTimeEnd = new Date().getTime();
+			$compiled.val(hex);
+			$compileTime.html('Compiled sketch in ' + (compileTimeEnd - compileTimeStart) + 'ms');
+			$submitButton.val("Compile Sketch").removeClass('compiling');
+		});
 	};
 
-	$scope.reset = function() {
-		$scope.user = angular.copy($scope.master);
-	};
-
-	$scope.reset();
 }
