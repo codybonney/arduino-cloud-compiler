59ca58ea41e4dbac9c4169d8c66e85a551212e34
Cody Bonney <me@codybonney.com>
2014-01-24 08:42:09 -0700

    Added watch for compiling

--- static/js/controller.js
+++ static/js/controller.js
@@ -1,16 +1,15 @@
 function Controller($scope) {
-	var submitButtonDefault = "Compile Sketch";
 	var defaultSketch = "#define LED_PIN 13\n\nvoid setup()\n{\n    pinMode(LED_PIN, OUTPUT);\n}\n\nvoid loop()\n{\n    digitalWrite(LED_PIN, HIGH);\n    delay(100);\n    digitalWrite(LED_PIN, HIGH);\n    delay(900);\n}";
 
+	$scope.compiling = false;
 	$scope.compileTime = "";
 	$scope.status = "waiting";
 	$scope.sketch = defaultSketch;
 	$scope.compiled = "";
-	$scope.submitButton = submitButtonDefault;
 
 	$scope.compile = function() {
+		$scope.compiling = true;
 		$scope.status = "compiling";
-		$scope.submitButton = "Compiling...";
 		$scope.compiled = "Compiling...";
 		var compileTimeStart = new Date().getTime();
 
@@ -22,18 +21,26 @@ function Controller($scope) {
 			}
 		})
 		.success(function(hex) {
+			$scope.compiling = false;
 			var compileTimeEnd = new Date().getTime();
 			$scope.compiled = hex;
 			$scope.compileTime = 'Compiled sketch in ' + (compileTimeEnd - compileTimeStart) + 'ms';
 			$scope.status = "success";
-			$scope.submitButton = submitButtonDefault;
 			$scope.$apply();
 		})
 		.error(function(msg) {
+			$scope.compiling = false;
 			$scope.compiled = "error";
 			$scope.status = "error";
-			$scope.submitButton = submitButtonDefault;
 			$scope.$apply();
 		});
 	};
+
+	$scope.$watch('compiling', function() {
+		if($scope.compiling) {
+			$scope.submitButton = "Compiling...";
+		} else {
+			$scope.submitButton = "Compile Sketch";
+		}
+	});
 }
